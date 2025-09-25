5fcc13ae71b10e065ec857fc333b3c006cca90da
jzhang <jinping4038@gmail.com>
2025-09-25 11:23:18 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jin Zhang on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
